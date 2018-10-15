32ba5e1df6510a928bc48eeeab6aad94efd98f21
BitcoinUltima <bitcoinultimate1511@gmail.com>
2018-10-15 13:28:35 +0200

    Damca1

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitcoinUltimate-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitcoinUltimateURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
